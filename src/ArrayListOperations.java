e30c585c18ce6f1f41a44c53904c6e4a70896d7e
Amrita Reji <amritareji@Amritas-MacBook-Pro.local>
2026-02-28 21:30:47 +0530

    hi

--- src/ArrayListOperations.java
+++ src/ArrayListOperations.java
@@ -5,6 +5,29 @@ import java.util.Scanner;
 public class ArrayListOperations {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        
+
+        int N = scanner.nextInt();
+        scanner.nextLine(); 
+
+        ArrayList<String> list = new ArrayList<>();
+
+        String[] items = scanner.nextLine().split(" ");
+        for (int i = 0; i < N; i++) {
+            list.add(items[i]);
+        }
+
+        Collections.sort(list);
+
+        String searchItem = scanner.nextLine();
+
+        System.out.println("Sorted Items: " + list);
+
+        if (list.contains(searchItem)) {
+            System.out.println("Found");
+        } else {
+            System.out.println("Not Found");
+        }
+
+        scanner.close();
     }
 }
